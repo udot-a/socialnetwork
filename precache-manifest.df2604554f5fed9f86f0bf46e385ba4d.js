1b26a553eae2f80151a357a44abd6f600541e9ec
udot_a <udot.dron@gmail.com>
2020-04-13 11:16:54 +0300

    First release

---
+++ precache-manifest.df2604554f5fed9f86f0bf46e385ba4d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b600bf0383801842ee9a00f5db0f7c60",
+    "url": "/MyReactProject/index.html"
+  },
+  {
+    "revision": "655c45bbab396aa69c96",
+    "url": "/MyReactProject/static/css/3.a2cf68d4.chunk.css"
+  },
+  {
+    "revision": "c8240ef2a7962747a97d",
+    "url": "/MyReactProject/static/css/4.cf5779db.chunk.css"
+  },
+  {
+    "revision": "488338f3f411e2cc8ace",
+    "url": "/MyReactProject/static/css/main.406c7e35.chunk.css"
+  },
+  {
+    "revision": "a740eddb96fb7e9d74de",
+    "url": "/MyReactProject/static/js/2.6d6e6b4d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/MyReactProject/static/js/2.6d6e6b4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "655c45bbab396aa69c96",
+    "url": "/MyReactProject/static/js/3.07216689.chunk.js"
+  },
+  {
+    "revision": "c8240ef2a7962747a97d",
+    "url": "/MyReactProject/static/js/4.6627c31d.chunk.js"
+  },
+  {
+    "revision": "488338f3f411e2cc8ace",
+    "url": "/MyReactProject/static/js/main.c99222ca.chunk.js"
+  },
+  {
+    "revision": "23a54ca03ea565432d6d",
+    "url": "/MyReactProject/static/js/runtime-main.2ad9532b.js"
+  },
+  {
+    "revision": "2096574fdb2c45187bff40aeef18a10d",
+    "url": "/MyReactProject/static/media/Pacman-1s-200px.2096574f.svg"
+  },
+  {
+    "revision": "4802d74b61e6536af5fc5cc6c51189b6",
+    "url": "/MyReactProject/static/media/user.4802d74b.jpg"
+  }
+]);
